0df9088823d6c5c13f77f9162476413e560967de
Renlov <a.pimenov95@mail.ru>
2021-01-03 14:24:08 +0300

    Location layout

--- app/src/main/java/com/example/locationapi/MainActivity.java
+++ app/src/main/java/com/example/locationapi/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.locationapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startLocation, stopLocation;
+    private TextView locationTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startLocation = findViewById(R.id.startLocation);
+        stopLocation = findViewById(R.id.stopLocation);
+        locationTextView = findViewById(R.id.locationTextView);
+
+
+
     }
 }
