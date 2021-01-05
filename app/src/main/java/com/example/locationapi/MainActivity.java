c3db2cd7699a1067ade64e2e699dc1eea72fafea
Renlov <a.pimenov95@mail.ru>
2021-01-05 10:42:09 +0300

    startLocationUpdates()

--- app/src/main/java/com/example/locationapi/MainActivity.java
+++ app/src/main/java/com/example/locationapi/MainActivity.java
@@ -1,19 +1,34 @@
 package com.example.locationapi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -22,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button startLocation, stopLocation;
     private TextView locationTextView, locationUpdateTimeTextView;
+    private static final int CHECK_SETTING_CODE = 111;
 
     //Класс для определения местоположения
     private FusedLocationProviderClient fusedLocationClient;
@@ -33,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     private LocationSettingsRequest locationSettingsRequest;
     //Используется для событий определения местоположения
     private LocationCallback locationCallback;
+    //Класс, в котором храниться широта и долгота
     private Location currentLocation;
 
     private boolean isLocationActive;
@@ -52,11 +69,66 @@ public class MainActivity extends AppCompatActivity {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         settingsClient = LocationServices.getSettingsClient(this);
 
+        startLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLocationUpdate();
+            }
+        });
+
+
         buildLocationRequest();
         buildLocationCallBack();
         buildLocationSettingRequest();
 
 
+    }
+
+    private void startLocationUpdate() {
+
+        isLocationActive = true;
+        startLocation.setEnabled(false);
+        stopLocation.setEnabled(true);
+
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+                .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            return;
+                        }
+                        fusedLocationClient.requestLocationUpdates(locationRequest,
+                                locationCallback, Looper.myLooper());
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                int statusCode = ((ApiException) e).getStatusCode();
+                switch (statusCode) {
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            ResolvableApiException resolvableApiException =
+                                    (ResolvableApiException) e;
+                            resolvableApiException.startResolutionForResult(
+                                    MainActivity.this, CHECK_SETTING_CODE
+                            );
+                        } catch (IntentSender.SendIntentException sendIntentException) {
+                            sendIntentException.printStackTrace();
+                        }
+                        break;
+
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        String message = "Make location settings on your device";
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                        isLocationActive = false;
+                        startLocation.setEnabled(true);
+                        stopLocation.setEnabled(false);
+
+                }
+                updateLocationUi();
+            }
+        });
     }
 
     private void buildLocationSettingRequest() {
@@ -72,13 +144,16 @@ public class MainActivity extends AppCompatActivity {
                 super.onLocationResult(locationResult);
                 currentLocation = locationResult.getLastLocation();
 
-                locationTextView.setText("" + currentLocation.getLatitude() +
-                        "/" + currentLocation.getLongitude());
-
-                locationUpdateTimeTextView.setText(DateFormat.getTimeInstance().format(new Date()));
-            }
+                updateLocationUi();
+    }
         };
+    }
 
+    private void updateLocationUi() {
+        locationTextView.setText("" + currentLocation.getLatitude() +
+                "/" + currentLocation.getLongitude());
+
+        locationUpdateTimeTextView.setText(DateFormat.getTimeInstance().format(new Date()));
     }
 
     private void buildLocationRequest() {
